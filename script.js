6299eafaddec95f6db2e4c63a899c2db5edcc12a
Saleamlack <saleamlack7@gmail.com>
2022-12-02 14:18:39 +0300

    Write startGame() which adds event listener
    startGame function add event listener and execute
    other functions when the event happened.
    functions called when the event happen
            - passSelection: get the target element and passed it ass
              playerSelection.
    	- styleSelection: change the background color of selected
    	  elements.
    	- appendMessage: add 'div' element for the message and insert
    	  it before 'footer' element in the body of the document.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getComputerChoice() {
         case (2):
             return ("paper");
         case (3):
-            return ("scissor");
+            return ("scissors");
     }
 }
 
@@ -21,21 +21,21 @@ function playRound(playerSelection, computerSelection) {
         switch (computerSelection) {
             case ("paper"):
                 return ("You Lose! Paper beats Rock.");
-            case ("scissor"):
+            case ("scissors"):
                 return ("You Win! Rock crushes Scissors.")
         }
     } else if (playerSelection === "paper") {
         switch (computerSelection) {
             case ("rock"):
                 return ("You Win! Paper beats Rock.");
-            case ("scissor"):
+            case ("scissors"):
                 return ("You Lose! Scissors cuts Paper.")
         }
-    } else if (playerSelection === "scissor") {
+    } else if (playerSelection === "scissors") {
         switch (computerSelection) {
             case ("rock"):
                 return ("You Lose! Rock crushes Rock.");
-            case ("Paper"):
+            case ("paper"):
                 return ("You Win! Scissor cuts paper.")
         }
     } else {
@@ -43,3 +43,49 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+//change background color of selected element
+function styleSelection(playerSelection, computerSelection) {   
+    const BLUE = "rgb(61, 61, 228)";
+    const GREEN = "rgba(43, 255, 0, 0.884)";
+    let computerSelections = Array.from(document.querySelectorAll(".computerSelection"));
+    let playerSelections = Array.from(document.querySelectorAll(".playerSelection"));
+
+    computerSelections.forEach(selection => selection.style.backgroundColor = "");
+    playerSelections.forEach(selection => selection.style.backgroundColor = "");
+
+    playerSelection.style.backgroundColor = GREEN;
+    computerSelections.forEach(selection => {
+        if (selection.classList[1] === computerSelection) selection.style.backgroundColor = BLUE;
+    });
+}
+//add div for message
+function appendMessage(playerSelection, computerSelection) {
+    const messageDiv = document.createElement('div');
+    let children = Array.from(document.body.children);
+    children.forEach(child => {
+        if (child.classList.value === 'message') document.body.removeChild(child)
+    });
+    messageDiv.classList.add('message');
+    let message = String(playRound(playerSelection.classList[1], computerSelection));
+    let text = document.createTextNode(message);
+    let footer = document.querySelector('footer');
+
+    messageDiv.style.cssText = "text-align: center; margin: 5px 0px;";
+    messageDiv.appendChild(text);
+    document.body.insertBefore(messageDiv, footer);
+}
+//pass player selection for other functions
+function passSelection(event) {
+    let computerSelection = getComputerChoice();
+    styleSelection(event.target, computerSelection);
+    appendMessage(event.target, computerSelection);
+    event.stopPropagation();
+}
+
+//start the game
+function startGame() {
+    const selections = Array.from(document.querySelectorAll(".playerSelection"));
+    selections.forEach(selection => selection.addEventListener('click', passSelection));
+}
+
+startGame();
